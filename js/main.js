f0c5d901bc8bdbe59bdc9f4089bb6290f5ec97d6
paulvvo <paulvo@live.ca>
2017-10-22 21:44:04 -0400

    added delete button, added validationfor url check

--- js/main.js
+++ js/main.js
@@ -17,6 +17,10 @@ function saveBookmark(e){
     var siteName = document.getElementById('siteHolder').value;
     var siteUrl = document.getElementById('siteURL').value;
 
+    if(!validation(siteUrl, siteName)){
+        return false;
+    }
+    
     var bookmark = {
         name:siteName,
         url:siteUrl
@@ -61,6 +65,11 @@ function saveBookmark(e){
         localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
      }
 
+    //clear form
+    document.getElementById("myForm").reset();
+    
+    //fetch bookmarks to update the page
+    fetchBookmarks();
     e.preventDefault();
 }
 
@@ -86,10 +95,49 @@ function fetchBookmarks(){
 
         bookmarksResults.innerHTML += '<div class="well">' +
                                       '<h3>' + name +
-                                      '<a class="btn btn-primary" target="_blank" href="' + url + '"> Visit </a> ' +
-                                      '</h3>' +
+                                      ' <a class="btn btn-primary" target="_blank" href="' + url + '">  Visit </a> ' +
+                                      ' <a onclick = "deleteBookmark(\''+url+'\')"class="btn btn-danger" href="#">  Delete </a> ' +
+                                      ' </h3>' +
                                       '</div>';
     }
     
 
+}
+
+function deleteBookmark(url){
+        //retrieve the bookmarks from local storage
+        var bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+
+        //cycle through the entire bookmark array
+        for(var i=0; i< bookmarks.length; i++){
+            //match delete bookmark with bookmark in array
+            if(bookmarks[i].url == url){
+                //remove the bookmark from the array
+                bookmarks.splice(i,1);
+            }
+        }
+        //set the array of bookmarks back into localstorage
+        localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+        //fetchbookmarks to refresh the page
+        fetchBookmarks();
+    
+}
+
+
+function validation(siteUrl,siteName){
+    var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+    var regex = new RegExp(expression);
+
+    //checks to see if there is a submission
+    if(!siteName || !siteUrl){
+        alert("Please Fill in the Form");
+        return false;
+    }
+    //determines whether the submissions is a valid url
+    if(!siteUrl.match(regex)){
+        alert("not valid url");
+        return false;
+    }
+
+    return true;
 }
